89fb43fed34207d20e5b36433b5c8b5350832719
Brotin Halder <128289788+Brotin27@users.noreply.github.com>
2023-04-20 23:19:32 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,40 +1,58 @@
 class script(object):
-    START = """**Hello {} 🤟**
+
+    START = """**Hello {} 🤟bro whatsup**
+
    
+
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
+
 I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
 
     HELP = """To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
+
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @brotinhalder.
+
 - After verification send /connect YourChannelID
+
 - Example : /connect -100xxxxxxxxxx
+
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
+
 this will help you to remove a indexed channel from your group.
 
 Get connected channels list with - /connections"""
 
-    ABOUT = """Developed By @CyniteBackup
+    ABOUT = """Developed By @brotinhalder
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
-✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
-✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
-✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+
+✯ Cʀᴇᴀᴛᴏʀ: 😁
+
+✯ Lᴀɴɢᴜᴀɢᴇ: 😁
+
+✯ DᴀᴛᴀBᴀsᴇ: 😁"""
 
     STATS = """My Status 💫
 
 👥 Users: {}
+
 🧿 Groups: {}"""
 
     BROADCAST = """<u>{}</u>
 
 Total: `{}`
+
 Remaining: `{}`
+
 Success: `{}`
+
 Failed: `{}`"""
+
+    
+
 
     
