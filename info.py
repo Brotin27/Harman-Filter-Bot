fe415c30584215a2c5899b9a3b5ee981e0b717a6
Brotin Halder <128289788+Brotin27@users.noreply.github.com>
2023-04-20 23:46:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 152006567186
 API_HASH     = "de70bc10da0a35fbd152006567186995"
 BOT_TOKEN    = "6196726931:AAEbNvcLaA1ZP5dv1nhDld5L9f-UQHws668"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://tinytmpgzxjy:uWD20jsomv2oSBFx@cluster0.o1kjri6.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001984713202
 ADMIN        = 1623892821
 CHANNEL      = "@B3botofficial"
